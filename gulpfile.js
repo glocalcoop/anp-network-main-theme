a61f1414e23f41ff488e8e856ff93bb03d6f661a
Pea Lutz <pea@glocal.coop>
2015-12-11 21:13:51 -0500

    Updated gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
-// npm install --save-dev gulp gulp-plumber gulp-watch gulp-livereload gulp-minify-css gulp-jshint jshint-stylish gulp-uglify gulp-rename gulp-notify gulp-include gulp-sass
+// npm install --save-dev gulp gulp-plumber gulp-watch gulp-minify-css gulp-jshint jshint-stylish gulp-uglify gulp-rename gulp-notify gulp-include gulp-sass
 
 var gulp = require('gulp'),
 	plumber = require( 'gulp-plumber' ),
 	watch = require( 'gulp-watch' ),
-	livereload = require( 'gulp-livereload' ),
 	minifycss = require( 'gulp-minify-css' ),
-	// jshint = require( 'gulp-jshint' ),
-	// stylish = require( 'jshint-stylish' ),
-	uglify = require( 'gulp-uglify' ),
 	rename = require( 'gulp-rename' ),
 	notify = require( 'gulp-notify' ),
 	include = require( 'gulp-include' ),
@@ -32,40 +28,14 @@ gulp.task( 'styles', function() {
 	.pipe( minifycss() )
 	.pipe( rename( { suffix: '.min' } ) )
 	.pipe( gulp.dest( './library/css' ) )
-	.pipe( livereload() )
 	.pipe( notify({ message: 'Styles task complete' }));
 } );
-
-// Scripts
-// gulp.task('scripts', function() {
-//   return gulp.src( 'src/js/**/*.js' )
-//     .pipe( concat( 'main.js') )
-//     .pipe( gulp.dest( 'assets/js' ) )
-//     .pipe( rename( { suffix: '.min' } ) )
-//     .pipe( uglify() )
-//     .pipe( gulp.dest('assets/js') )
-//     .pipe( notify( { message: 'Scripts task complete' } ) );
-// });
-
 
 // Watch
 gulp.task('watch', function() {
 
 	// Watch .scss files
 	gulp.watch('library/sass/**/*.scss', ['styles']);
-
-	// Watch .js files
-	//gulp.watch('library/js/**/*.js', ['scripts']);
-
-	gulp.watch( './**/*.php' ).on( 'change', function( file ) {
-		livereload.changed( file );
-	} );
-
-	// Create LiveReload server
-	livereload.listen();
-
-	// Watch any files in dist/, reload on change
-	gulp.watch(['library/**']).on('change', livereload.changed);
 
 });
 
