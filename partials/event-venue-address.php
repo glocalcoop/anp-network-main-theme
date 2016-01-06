61261c5b853b9880de05fee76572fe2f7ca78629
Pea Lutz <pea@glocal.coop>
2016-01-05 20:15:42 -0500

    Moved Address heading out of partial.

--- partials/event-venue-address.php
+++ partials/event-venue-address.php
@@ -26,23 +26,20 @@
 
 <?php if( eo_get_venue() ) : ?>
 
-	<h3><?php _e( 'Address', 'anp-network-main' ); ?></h3>
 	<?php $venue_id = eo_get_venue( get_the_ID() ); ?>
 	<?php $venue_address = eo_get_venue_address( $venue_id ); ?>
 	<?php $venue_link = eo_get_venue_link( $venue_id ); ?>
 	<div class="event-location">
-		<span class="location-address">
-			<?php if( !is_tax( 'event-venue' ) ) : ?>
-			<span class="location-name"><a href="<?php echo esc_url( $venue_link ); ?>"><?php echo eo_get_venue_name( $venue_id ); ?></a></span>
-			<?php endif; ?>
-	 		<span class="street-address"><?php echo $venue_address['address']; ?></span>
-	 		<span class="city-state-postalcode">
-		 		<span class="city"><?php echo $venue_address['city']; ?></span>
-		 		<span class="state"><?php echo $venue_address['state']; ?></span>
-		 		<span class="postal-code"><?php echo $venue_address['postcode']; ?></span>
-	 		</span>
-	 		<span class="country"><?php echo $venue_address['country']; ?></span>
-		</span>
+		<?php if( !is_tax( 'event-venue' ) ) : ?>
+		<span class="location-name"><a href="<?php echo esc_url( $venue_link ); ?>"><?php echo eo_get_venue_name( $venue_id ); ?></a></span>
+		<?php endif; ?>
+ 		<span class="street-address"><?php echo $venue_address['address']; ?></span>
+ 		<span class="city-state-postalcode">
+	 		<span class="city"><?php echo $venue_address['city']; ?></span>
+	 		<span class="state"><?php echo $venue_address['state']; ?></span>
+	 		<span class="postal-code"><?php echo $venue_address['postcode']; ?></span>
+ 		</span>
+ 		<span class="country"><?php echo $venue_address['country']; ?></span>
 	</div>
 
 <?php endif; ?>
